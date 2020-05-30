177eba5869080c21c4c19f1b8a8a472a8d393265
shrutiSingh2601 <66126147+shrutiSingh2601@users.noreply.github.com>
2020-05-30 14:18:01 +0530

    Create program3.py

---
+++ day1/program3.py
@@ -0,0 +1,11 @@
+num1 = input("enter number 1")
+num2 = input("enter number 2")
+print("before swapping")
+print("num1=" , num1)
+print("num2=" , num2)
+num1 = num1 + num2
+num2 = num1 - num2
+num1 = num1-num2
+print("after swapping")
+print("num1=" , num1)
+print("num2=" , num2)
