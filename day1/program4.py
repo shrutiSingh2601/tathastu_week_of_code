8d68b491b8f9ac872997df6c3da9ad319ec87847
shrutiSingh2601 <66126147+shrutiSingh2601@users.noreply.github.com>
2020-05-30 14:26:59 +0530

    Create program4.py

---
+++ day1/program4.py
@@ -0,0 +1,6 @@
+cp = input("enter the cost price")
+sp = input("enter the selling price")
+profit = sp - cp
+newsp = 1.05 * profit + cp
+print("profit is:" , profit)
+print("selling price for increase in profit by 5% is:" , newsp )
